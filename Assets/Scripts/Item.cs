cec2f7e1af64ed0a71f6436f8475737dbabadb73
Luis Torres <louie.torres.e@gmail.com>
2025-02-06 21:07:50 -0500

    Challenge2 Review Item Script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,8 +8,15 @@ public class Item : MonoBehaviour
     //every item has a description
     //every item has an image
 
+    public string itemName;
+    public string description;
+    public Sprite sprite;
+    public int attack;
+
     void Start()
     {
-        
+        print("Name: " + itemName);
+        print("Description: " + description);
+        print("Attack: " + attack);
     }
 }
