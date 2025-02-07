6b8f02ba825dea2bb0d85c4e675ad50e65154fa5
Luis Torres <louie.torres.e@gmail.com>
2025-02-06 21:04:22 -0500

    Challenge 2 Item Script

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //ever item has a name
+    //every item has a description
+    //every item has an image
+
+    void Start()
+    {
+        
+    }
+}
