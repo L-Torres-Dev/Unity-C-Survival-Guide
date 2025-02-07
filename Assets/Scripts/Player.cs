0e18439fd4ccb9feb6e50bc18bc2d43bee80a9a4
Luis Torres <louie.torres.e@gmail.com>
2025-02-06 20:48:03 -0500

    variable Basics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    /// <summary> What a variable needs:
+    /// 1. public of private reference
+    /// 2. data type (string, bool, float, int)
+    /// 3. name of variable
+    /// 4. optional -- a value
+    /// </summary>
+
+    //variable for my name
+    //variable for my age
+    //variable for my location
+    public string myName = "Luis";
+    public int myAge = 23;
+    private string location = "USA"; //I don't want the rest of the world to know my location!!
+
     void Start()
     {
-        
+        print("My name is: " + myName);
+        print("My age is: " + myAge);
     }
 
     // Update is called once per frame
